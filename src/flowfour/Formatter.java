f8235cb20e94453fb7c31cf4834d4b2c61c49618
jhl92 <thomasnub@hotmail.com>
2013-11-15 12:06:37 +0100

    0.0.1.4

--- src/flowfour/Formatter.java
+++ src/flowfour/Formatter.java
@@ -7,55 +7,106 @@ package flowfour;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Simon
  */
-public class Formatter {
+public class Formatter
+{
 
     private FileWriter fw;
     private Scanner scan;
     private String path;
     private String fileName;
+    private ArrayList<String> wordPairs = new ArrayList<>();
 
-    public Formatter() {
+    ;
+
+    
+    public Formatter()
+    {
         setup();
     }
 
-    
-    private void setup(){
-       String dir = System.getProperty("user.dir"); 
-       fileName = "ordbog";
-       path = dir+"/"+"ordbog"+"/"+fileName;
+    private void setup()
+    {
+        String dir = System.getProperty("user.dir");
+        fileName = "ordbog";
+        path = dir + "/" + "ordbog" + "/" + fileName;
     }
-    
-    
-    public void saveToFile() {
-        try {
 
-            fw = new FileWriter(new File("lol"));
+    public boolean saveToFile()
+    {
+        try
+        {
+
+            fw = new FileWriter("lol");
+            for (String str : wordPairs)
+            {
+                fw.write(str);
+            }
+            fw.close();
+            System.out.println(fileName + " has been saved");
 
 
-
-        } catch (Exception ex) {
+        } catch (Exception ex)
+        {
             System.out.println("error" + ex);
+            return false;
         }
+        return true;
     }
 
-    public void readFromFile() {
-        try {
+    public boolean readFromFile()
+    {
+        try
+        {
             scan = new Scanner(new File(path));
-            
-        
-        
-        } catch (FileNotFoundException ex) {
-            
+            while (scan.hasNext())
+            {
+                wordPairs.add(scan.next());
+            }
+            scan.close();
+            System.out.println(fileName + " has been loaded ");
+
+
+
+
+        } catch (FileNotFoundException ex)
+        {
+            System.out.println("error" + ex);
+            return false;
+
         }
-        
-        
+
+        return true;
+    }
+//    public boolean delete()
+//    {
+//        try
+//        {
+//            if (file.delete())
+//            {
+//                System.out.println(fileName + "is deleted");
+//            } else
+//            {
+//                System.out.println("Unable to delete" + fileName);
+//                return false;
+//            }
+//
+//        } catch (Exception e)
+//        {
+//
+//            e.printStackTrace();
+//
+//        }
+//        return true;
+//    }
+    public void clear()
+    {
+        wordPairs.clear();
     }
 }
