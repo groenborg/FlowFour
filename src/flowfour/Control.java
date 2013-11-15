3fef6d7bb0985ba53207ca16b824de9c1830f6f5
groenborg <groenborg27@gmail.com>
2013-11-15 11:26:25 +0100

    0.0.1.3

--- src/flowfour/Control.java
+++ src/flowfour/Control.java
@@ -4,7 +4,10 @@
  */
 package flowfour;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
+import javazoom.jl.decoder.JavaLayerException;
 
 /**
  *
@@ -21,46 +24,56 @@ public class Control implements WordPairControlIF {
         answer = answer.trim();
         char divider = ',';
         int difficulty = 0;
-        
         StringBuilder sb = new StringBuilder();
         sb.append(question).append(divider);
         sb.append(answer).append(divider);
         sb.append(difficulty);
         wordPairs.add(sb.toString());
         
-        
-       
-        
-        
         translator = new Translator();
+        
         try{
             translator.readOutLoud(answer);
         
-        }catch(Exception e){
+        }catch(IOException | JavaLayerException e){
+            
         }
-        
-        
         System.out.println(wordPairs.get(difficulty));
     }
 
     @Override
     public boolean delete(String question) {
+        
+        // følg op på det her
+        
+        for(int x = 0; x < wordPairs.size();++x){
+            String wp = wordPairs.get(x);
+            String[] a = wp.split(",");
+            if(a[0].equalsIgnoreCase(question)){
+                wordPairs.remove(x);
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 1;
+        return wordPairs.size();
     }
 
     @Override
     public String getRandomQuestion() {
-        return "";
+        // følg op på den her
+        Random r = new Random();
+        return wordPairs.get(r.nextInt(size()));
     }
 
     @Override
     public boolean checkGuess(String question, String quess) {
         return true;
+        
+        
     }
 
     @Override
